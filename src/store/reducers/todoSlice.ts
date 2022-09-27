6cb6f5c68a62c536bb54258c7846d6ecd297fb4b
ExistencePersik <prostor130@gmail.com>
2022-09-27 18:57:23 +0700

    LocalStorage for MainPage

--- src/store/reducers/todoSlice.ts
+++ src/store/reducers/todoSlice.ts
@@ -1,12 +1,14 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import { ITodos } from '../../models/models'
 
+const MY_TASK = 't'
+
 type TodosState = {
   list: ITodos[]
 }
 
 const initialState: TodosState = {
-  list: []
+  list: JSON.parse(localStorage.getItem(MY_TASK) ?? '[]')
 }
 
 const todoSlice = createSlice({
@@ -19,15 +21,18 @@ const todoSlice = createSlice({
         title: action.payload,
         completed: false,
       })
+      localStorage.setItem(MY_TASK, JSON.stringify(state.list))
     },
     toggleComplete(state, action: PayloadAction<string>) {
       const toggledTodo = state.list.find(todo => todo.id === action.payload);
       if (toggledTodo) {
         toggledTodo.completed = !toggledTodo.completed;
       }
+      localStorage.setItem(MY_TASK, JSON.stringify(state.list))
     },
     removeTodo(state, action: PayloadAction<string>) {
-      state.list = state.list.filter(todo => todo.id !== action.payload);
+      state.list = state.list.filter(todo => todo.id !== action.payload)
+      localStorage.setItem(MY_TASK, JSON.stringify(state.list))
     },
     editTodo(state, action) {
 
